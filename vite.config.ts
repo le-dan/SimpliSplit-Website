e66ddf64b15db7603eeba1bdff5aba06336a6e6b
Dan Le <tle57@gmu.edu>
2023-09-16 21:11:15 -0400

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: "/SimpliSplit-Website",
+	base: "/SimpliSplit-Website/",
 	plugins: [vue()],
 	resolve: {
 		alias: {
